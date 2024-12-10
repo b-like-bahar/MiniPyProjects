e847cad81b8aeeed42cfe69262ba04403e033c13
Bahar Hamzeh <blikebahar76@gmail.com>
2024-12-10 17:23:45 -0600

    add a initial line for better user experience

--- Guess a number/GAN_User.py
+++ Guess a number/GAN_User.py
@@ -3,7 +3,8 @@ import random
 def guess_game_user():
     minimum = 1
     maximum = 1000
-    feedback = ''
+    print(f"Think of a number between {minimum} and {maximum}, and I'll try to guess it!")
+
     while True:
         guess_number = random.randint(minimum,maximum)
         feedback = input(f"Is {guess_number} too high (H), too low (L), or correct (C) ?").lower()
