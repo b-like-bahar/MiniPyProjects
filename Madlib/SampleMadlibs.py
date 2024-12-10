d7194a3f731cc634a6df8794c00fa60a36a1dc85
Bahar Hamzeh <blikebahar76@gmail.com>
2024-12-10 13:53:23 -0600

    Create SmapleMadlibs module file

---
+++ Madlib/SampleMadlibs.py
@@ -0,0 +1,44 @@
+def adventure():
+    return(
+        "Once upon a time, a brave {1st_adjective} {1st_noun} set off on an exciting adventure. "
+        "With a/an {2nd_adjective} {2nd_noun} in hand, they traveled through the {3rd_adjective} forest. "
+        "Suddenly, they heard a loud {3rd_noun} and saw a/an {4th_adjective} {1st_animal}. Without hesitation, "
+        "they used their {4th_noun} to {1st_verb} it and continued on their way to the {5th_adjective} castle."
+    )
+
+def party():
+    return (
+        "It was the night of the grand {1st_adjective} party. Everyone was wearing {2nd_adjective} {1st_clothing} "
+        "and eating {1st_plural_noun}. The music was so {3rd_adjective} that everyone started to {1st_verb}. "
+        "Then, someone {2nd_verb} a huge {1st_noun} into the air, and the crowd cheered! But just as things were "
+        "getting fun, a/an {4th_adjective} {1st_animal} appeared and {3rd_verb} the entire {2nd_noun}. "
+        "Everyone laughed and decided to dance the night away!"
+    )
+
+def mystery():
+    return (
+        "Detective {1st_name} was called to investigate a mysterious {1st_noun} in the {1st_adjective} house on {1st_street_name}. "
+        "The {2nd_noun} was missing, and the only clue was a/an {2nd_adjective} {1st_object}. As they examined the scene, "
+        "they noticed a strange {3rd_noun} on the floor. 'This is no ordinary {4th_noun},' said Detective {2nd_name}. "
+        "'I think the suspect is a/an {3rd_adjective} {1st_animal}.' The case was just beginning to unfold..."
+    )
+
+def school_day():
+    return(
+     "On the first day of school, {1st_name} arrived at {1st_school_name} with a/an {1st_adjective} {1st_noun}."
+     "Their teacher, Mrs. {2nd_name}, was holding a/an {2nd_noun} and smiled warmly."
+     "Today, we will learn about {3rd_noun}, she said.During lunch, {3rd_name} ate a/an {2nd_adjective} sandwich with {4th_noun} and {5th_noun}."
+     "The day ended with a/an {3rd_adjective} class on {1st_subject}, and {4th_name} couldn't wait to return the next day."
+    )
+
+def robot():
+    return(
+    "In a laboratory deep underground, a/an {1st_adjective} robot named {1st_name} was created."
+    "It had a/an {2nd_adjective} {1st_noun} for a head and {3rd_adjective} {2nd_noun} for arms."
+    "It was programmed to {1st_verb} and {2nd_verb} every day, but one day, it began to feel {4th_adjective}."
+    "The robot decided to escape and visit the {5th_adjective} {1st_place}, where it would {3rd_verb} with {3rd_noun} for the rest of its life."
+    "The future looked {6th_adjective} for the robot."
+    )
+
+
+
