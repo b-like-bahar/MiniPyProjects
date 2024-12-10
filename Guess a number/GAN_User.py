8f243ae5ee2c65f20ee70eb28d4d4eadd24ea965
Bahar Hamzeh <blikebahar76@gmail.com>
2024-12-10 17:48:27 -0600

    add an if statement to check the validity of the user inputs

--- Guess a number/GAN_User.py
+++ Guess a number/GAN_User.py
@@ -8,11 +8,15 @@ def guess_game_user():
     while True:
         guess_number = random.randint(minimum,maximum)
         feedback = input(f"Is {guess_number} too high (H), too low (L), or correct (C) ?").lower()
-        if feedback == 'l':
-            minimum = guess_number+1
-        elif feedback == 'h':
-            maximum = guess_number-1
-        else :
-            print(f"Yay, the correct number is {guess_number}")
+        if feedback in ["l", "h", "c"]:
+            if feedback == 'l':
+                minimum = guess_number+1
+            elif feedback == 'h':
+                maximum = guess_number-1
+            else :
+                print(f"Yay, the correct number is {guess_number}")
+                break
+        else:
+            print("It seems like something went wrong with your feedback. Please run the program again!")
             break
 guess_game_user()
