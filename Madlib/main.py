14650f255cc481716929ec84001779693887f89f
Bahar Hamzeh <blikebahar76@gmail.com>
2024-12-10 14:41:16 -0600

    Add main function to handle user interaction and madlib selection

--- Madlib/main.py
+++ Madlib/main.py
@@ -15,3 +15,36 @@ def get_user_inputs(temp):
         user_input = input(f"Please enter {placeholder.replace('_', ' ')}: ")
         inputs[placeholder]= user_input
     return inputs
+
+def main():
+    while True:
+        print("There are 5 mad libs in this list :")
+        print("1. The Adventure")
+        print("2. The Party")
+        print("3. The Mystery")
+        print("4. The Superhero Story")
+        print("5. The Robot")
+        choice = input("Please choose a madlib by its number(1, 2, 3, 4, or 5: ")
+        if choice == "1":
+            template = sm.adventure()
+            break
+        elif choice == "2":
+            template = sm.party()
+            break
+        elif choice == "3":
+            template = sm.mystery()
+            break
+        elif choice == "4":
+            template = sm.superhero_story()
+            break
+        elif choice == "5":
+            template = sm.robot()
+            break
+        else:
+            print("Invalid option, Try again.")
+            continue
+    user_inputs=get_user_inputs(template)
+    print(f"Here is your madlib:\n{template.format(**user_inputs)}")
+
+if __name__ == "__main__":
+    main()
