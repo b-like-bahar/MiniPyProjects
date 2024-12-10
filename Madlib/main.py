b33f47c316f49437b10f016004dfac843cb447b9
Bahar Hamzeh <blikebahar76@gmail.com>
2024-12-10 13:57:35 -0600

    Add get_user_inputs function to handle user input for madlib placeholders

---
+++ Madlib/main.py
@@ -0,0 +1,17 @@
+import SampleMadlibs as sm
+
+def get_user_inputs(temp):
+    # Extract placeholders in the correct order
+    placeholders = []
+    clean_word=''
+    for word in temp.split():
+
+        if word.startswith("{") or word.endswith("}."):
+            clean_word = word.strip("{}.")
+            placeholders.append(clean_word)
+
+    inputs = {}
+    for placeholder in placeholders:
+        user_input = input(f"Please enter {placeholder.replace('_', ' ')}: ")
+        inputs[placeholder]= user_input
+    return inputs
