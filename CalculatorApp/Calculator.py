69aae8cd123006eb5273cf149bf12625e496019d
Bahar Hamzeh <blikebahar76@gmail.com>
2024-12-25 16:15:59 -0700

    add get_number function to insert clicked numbers on calculator input field

--- CalculatorApp/Calculator.py
+++ CalculatorApp/Calculator.py
@@ -1,18 +1,25 @@
 from tkinter import *
 
 root = Tk()
+
+index=0
+def get_number(num):
+    global index
+    display.insert(index, num)
+    index+=1
+
 display = Entry(root)
 display.grid(row=1,columnspan=6)
 
 # number buttons
-counter1 =1
+numbers = [1,2,3,4,5,6,7,8,9]
+counter1 = 0
 for x in range(3):
     for y in range(3):
-        num_button = Button(root,text=counter1, width=2, height=2)
+        num_button = Button(root,text=numbers[counter1], width=2, height=2, command = lambda text=numbers[counter1]:get_number(text))
         num_button.grid(row=x+2, column=y)
         counter1+=1
-
-zero_button = Button(root, text="0", width=2, height=2)
+zero_button = Button(root, text="0", width=2, height=2, command= lambda : get_number(0))
 zero_button.grid(row=5, column=1)
 
 #operator buttons
@@ -28,3 +35,5 @@ for x in range(4):
 
 
 root.mainloop()
+
+
