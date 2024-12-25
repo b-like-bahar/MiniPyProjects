fce892e42aadd7931313ab42a6185a07a8689351
Bahar Hamzeh <blikebahar76@gmail.com>
2024-12-25 15:22:42 -0700

    add operators buttons to the calculator

--- CalculatorApp/Calculator.py
+++ CalculatorApp/Calculator.py
@@ -4,14 +4,27 @@ root = Tk()
 display = Entry(root)
 display.grid(row=1,columnspan=6)
 
-counter =1
+# number buttons
+counter1 =1
 for x in range(3):
     for y in range(3):
-        num_button = Button(root,text=counter, width=2, height=2)
+        num_button = Button(root,text=counter1, width=2, height=2)
         num_button.grid(row=x+2, column=y)
-        # num_button.pack()
-        counter+=1
+        counter1+=1
 
 zero_button = Button(root, text="0", width=2, height=2)
 zero_button.grid(row=5, column=1)
+
+#operator buttons
+operators = ["+", "-", "x", "/", "%", "^", "\u03C0" , "(", ")", "^2", "\u221A" ]
+
+counter2 = 0
+for x in range(4):
+    for y in range(3):
+        if counter2<len(operators):
+            operator_button = Button(root, text=operators[counter2], width=2, height=2)
+            operator_button.grid(row=x+2, column=y+3)
+            counter2+=1
+
+
 root.mainloop()
