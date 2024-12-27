7dc5a1defebd3720f80d1f3d87b066e368db50d1
Bahar Hamzeh <blikebahar76@gmail.com>
2024-12-26 23:31:34 -0700

    add turn label to the topof the game window

--- TicTacToe/tictactoe.py
+++ TicTacToe/tictactoe.py
@@ -20,4 +20,10 @@ root = Tk()
 root.title = ("Tic Tac Toe")
 root.resizable(False, False)
 
+frame = Frame(root)
+frame.pack()
+
+turn_label = Label(frame, text=f"{current_player}'s turn", font=("Helvetica", 20), bg=color_gray, fg="white")
+turn_label.grid(row=0, column=0, columnspan=3, sticky="we")
+
 root.mainloop()
