da7f9cb1706c666d0aef49d73c25e8c52f2ea0fe
Bahar Hamzeh <blikebahar76@gmail.com>
2024-12-28 03:00:29 -0700

    add Food class

--- SnakeGame/snakegame.py
+++ SnakeGame/snakegame.py
@@ -1,4 +1,17 @@
+from pty import spawn
 from tkinter import *
+import random
+
+class Food:
+    def __init__(self):
+        x = random.randint(0, (game_width / space_size)-1) * space_size
+        y = random.randint(0, (game_height / space_size) - 1) * space_size
+
+        self.coordinate = [x,y]
+        canvas.create_oval(x,y,x+space_size, y+space_size, fill=food_color)
+
+
+
 
 game_width = 700
 game_height = 700
@@ -9,7 +22,7 @@ body_pats = 2
 #colors
 snake_color = "#4584b6"
 food_color = "#ffde57"
-background_color = "000000"
+background_color = "#000000"
 
 
 
@@ -21,8 +34,11 @@ root.resizable(False, False)
 score = 0
 direction="down"
 
-score_label = Label(text="score:{}".format(score), font=("Helvetica", 20))
+score_label = Label(text="score : {}".format(score), font=("Helvetica", 20))
 score_label.pack()
 
+canvas = Canvas(root, bg=background_color, height=game_height, width=game_width)
+canvas.pack()
 
+food = Food()
 root.mainloop()
