716a686800d96fead2e6c6fe37f25dee7b412046
Bahar Hamzeh <blikebahar76@gmail.com>
2024-12-28 02:12:15 -0700

    define some initial variables

--- SnakeGame/snakegame.py
+++ SnakeGame/snakegame.py
@@ -1,5 +1,19 @@
 from tkinter import *
 
+game_width = 700
+game_height = 700
+speed = 50
+space_size = 50
+body_pats = 2
+
+#colors
+snake_color = "#4584b6"
+food_color = "#ffde57"
+background_color = "000000"
+
+
+
+# main window setup
 root = Tk()
 root.title("Snake Game")
 root.resizable(False, False)
